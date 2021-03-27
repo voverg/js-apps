8c6c92e87fe625ecd07ee9179286dfafe11d4e0c
voverg <voverg81@mail.ru>
2021-03-27 13:43:10 +0300

    Modify model.js in the main-project

--- main-project/model.js
+++ main-project/model.js
@@ -1,87 +1,45 @@
-const model = [
+const titles = [
+    'expanding-cards',
+    'progress-steps',
+    'rotating-navigation',
+    'hidden-search',
+    'blurry-loading',
+    'scroll-animation',
+    'form-input-wave',
+    'animated-navigation',
+    'event-keycode',
+    'faq-collaps',
+    'notes-app',
+    'drag-n-drop',
+    'svg-animation',
+    'piano',
+    'speed-reading-test',
+    'frontend-quiz',
+    'game',
+]
+
+function getData(list) {
+const resultList = list.map((item, index) => {
+        const titleItems = item.split('-');
+        titleItems[0] = titleItems[0][0].toUpperCase() + titleItems[0].slice(1);
+        const title = `${titleItems.join(' ')}`;
+        
+        const url = `./${item}/index.html`;
+        const img = `./main-project/img/${item}.png`;
+        
+        return {id: index, title, url, img};
+    });
+    
+    return resultList;
+}
+
+const model = getData(titles);
+
+
+/* const modelExample = [
     {
         title: 'Expanding cards',
         url: './expanding-cards/index.html',
         img: './main-project/img/expanding-cards.png'
     },
-    {
-        title: 'Progress steps',
-        url: './progress-steps/index.html',
-        img: './main-project/img/progress-steps.png'
-    },
-    {
-        title: 'Rotating navigation',
-        url: './rotating-navigation/index.html',
-        img: './main-project/img/rotating-navigation.png'
-    },
-    {
-        title: 'Hidden search',
-        url: './hidden-search/index.html',
-        img: './main-project/img/hidden-search.png'
-    },
-    {
-        title: 'Blurry loading',
-        url: './blurry-loading/index.html',
-        img: './main-project/img/blurry-loading.png'
-    },
-    {
-        title: 'Scroll animation',
-        url: './scroll-animation/index.html',
-        img: './main-project/img/scroll-animation.png'
-    },
-    {
-        title: 'Form input wave',
-        url: './form-input-wave/index.html',
-        img: './main-project/img/form-input-wave.png'
-    },
-    {
-        title: 'Animated navigation',
-        url: './animated-navigation/index.html',
-        img: './main-project/img/animated-navigation.png'
-    },
-    {
-        title: 'Event keyCode',
-        url: './event-keycode/index.html',
-        img: './main-project/img/event-keycode.png'
-    },
-    {
-        title: 'FAQ collaps',
-        url: './faq-collaps/index.html',
-        img: './main-project/img/faq-collaps.png'
-    },
-    {
-        title: 'Notes app',
-        url: './notes-app/index.html',
-        img: './main-project/img/notes-app.png'
-    },
-    {
-        title: 'Drag n Drop',
-        url: './drag-n-drop/index.html',
-        img: './main-project/img/drag-n-drop.png'
-    },
-    {
-        title: 'SVG Animation',
-        url: './svg-animation/index.html',
-        img: './main-project/img/svg-animation.png'
-    },
-    {
-        title: 'Piano',
-        url: './piano/index.html',
-        img: './main-project/img/piano.png'
-    },
-    {
-        title: 'Speed reading test',
-        url: './speed-reading-test/index.html',
-        img: './main-project/img/speed-reading-test.png'
-    },
-    {
-        title: 'Frontend quiz',
-        url: './frontend-quiz/index.html',
-        img: './main-project/img/frontend-quiz.png'
-    },
-    {
-        title: 'Game',
-        url: './game/index.html',
-        img: './main-project/img/game.png'
-    },
-]
+] */
