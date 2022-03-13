2115b74cdbcdf9c4714aad6033e9a354b494bac9
voverg <voverg81@mail.ru>
2022-03-13 16:43:44 +0300

    Add the habits project to the main project

--- main-project/model.js
+++ main-project/model.js
@@ -18,7 +18,8 @@ const titles = [
     'speed-reading-test',
     'frontend-quiz',
     'shine-board',
-    'split-landing-page'
+    'split-landing-page',
+    'habits',
 ]
 
 function getData(list) {
@@ -26,13 +27,13 @@ function getData(list) {
         const titleItems = item.split('-');
         titleItems[0] = titleItems[0][0].toUpperCase() + titleItems[0].slice(1);
         const title = `${titleItems.join(' ')}`;
-        
+
         const url = `./${item}/index.html`;
         const img = `./main-project/img/${item}.png`;
-        
+
         return {id: index, title, url, img};
     });
-    
+
     return resultList;
 }
 
