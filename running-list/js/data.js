e2a95bbc71dd6834bc4ca9d81579777e45721e05
voverg <voverg81@mail.ru>
2022-03-31 23:05:12 +0300

    Running list: modify the app design

---
+++ running-list/js/data.js
@@ -0,0 +1,45 @@
+const tasks = [
+  {
+    id: 1,
+    title: 'Тестовая задача',
+    category: 'тестовая',
+    status: {
+      mon: 'transfer',
+      tue: 'empty',
+      wen: 'new',
+      thu: 'doing',
+      fri: 'empty',
+      sat: 'empty',
+      sun: 'important',
+    },
+  },
+  {
+    id: 2,
+    title: 'Купить батон колбасы',
+    category: 'home',
+    status: {
+      mon: 'empty',
+      tue: 'empty',
+      wen: 'cancel',
+      thu: 'doing',
+      fri: 'empty',
+      sat: 'done',
+      sun: 'empty',
+    },
+  },
+];
+
+const category = {
+  'тестовая': 'red',
+  home: 'lightblue',
+};
+
+const status = {
+  empty: ' ',
+  new: 'app__task-day--new',
+  transfer: 'app__task-day--transfer',
+  doing: 'app__task-day--doing',
+  done: 'app__task-day--done',
+  important: 'app__task-day--important',
+  cancel: 'app__task-day--cancel',
+};
